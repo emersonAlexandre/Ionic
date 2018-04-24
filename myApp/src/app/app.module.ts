c9bcd85d0e6411a0fbadbcc764dfa068236e0462
dos Santos <emerson.d.dos.santos@accenture.com>
2018-04-24 17:50:32 -0300

    iniciando o provider da api movie

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -10,8 +10,10 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HttpModule } from '@angular/http';
 import { FeedPageModule } from '../pages/feed/feed.module';
 import { IntroPageModule } from '../pages/intro/intro.module';
+import { MoovieProvider } from '../providers/moovie/moovie';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,8 @@ import { IntroPageModule } from '../pages/intro/intro.module';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    HttpModule
   ],
   imports: [
     BrowserModule,
@@ -38,7 +41,8 @@ import { IntroPageModule } from '../pages/intro/intro.module';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MoovieProvider
   ]
 })
 export class AppModule {}
--- myApp/src/pages/feed/feed.ts
+++ myApp/src/pages/feed/feed.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MoovieProvider } from '../../providers/moovie/moovie';
 
 /**
  * Generated class for the FeedPage page.
@@ -12,8 +13,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
+  providers :[
+    MoovieProvider
+  ]
 })
 export class FeedPage {
+
+  objetoFeed = {
+    titulo: "Emerson Alexandre",
+    data: "May 7, 1996",
+    descricao: "Estou criando um app incrível",
+    qtd_likes: 12,
+    qtd_comments: 4,
+    time_comment: "11h ago"
+  }
   nomeUsuario:string = "Emerson Alexandre";
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -23,7 +36,7 @@ export class FeedPage {
   }
 
   ionViewDidLoad() {
-    this.somaDoisNumeros(10, 99);
+    // this.somaDoisNumeros(10, 99);
   }
 
 }
